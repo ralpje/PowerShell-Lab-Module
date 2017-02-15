0d9e78d6ca1d43e0a32ed6db1b70ce27d3b1ee35
Sven van Rijen <sven@svenvanrijen.nl>
2017-02-15 14:04:34 +0100

    Added and changed some template files

---
+++ Templates/Compile_Azure_DSC_Node.config.ps1
@@ -0,0 +1,41 @@
+﻿#region Azure Login
+
+Login-AzureRmAccount
+
+#endregion
+
+#region upload config
+
+
+
+Import-AzureRmAutomationDscConfiguration -SourcePath "<fill me in>" `
+                                         -Published `
+                                         -ResourceGroupName "<fill me in>" `
+                                         -AutomationAccountName "<fill me in>" `
+                                         -Force
+
+#endregion
+
+#region compile config
+
+$ConfigData = @{             
+    AllNodes = @(             
+        @{             
+            Nodename = "*"             
+        }
+
+        @{             
+            Nodename = "<fill me in>"
+            Role = "<fill me in>"             
+        }
+        
+       
+   )             
+}             
+
+Start-AzureRmAutomationDscCompilationJob -ResourceGroupName "<fill me in>" `
+                                         -AutomationAccountName "<fill me in>" `
+                                         -ConfigurationName "<fill me in>" `
+                                         -ConfigurationData $ConfigData
+
+#endregion
